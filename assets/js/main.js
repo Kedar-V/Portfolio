2057afe6ad6ec45cb64735a0d6d294bd06af5bba
Kedar-V <kedarvaidya0504@gmail.com>
2022-11-13 15:02:40 +0530

    Fixed Form

--- assets/js/main.js
+++ assets/js/main.js
@@ -281,17 +281,3 @@
 
 })()
 
-function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "sender@email_address.com",
-    Password: "Enter your password",
-    To: 'receiver@email_address.com',
-    From: "sender@email_address.com",
-    Subject: "Sending Email using javascript",
-    Body: "Well that was easy!!",
-  })
-    .then(function (message) {
-      alert("mail sent successfully")
-    });
-}
